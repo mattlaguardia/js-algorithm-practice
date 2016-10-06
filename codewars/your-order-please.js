cd2078cf5f7a22422aeba8ac3d6721472c5c80ec
mattlaguardia <matthewlaguardia@gmail.com>
2016-10-05 18:04:11 -0700

    finishes your order please

---
+++ codewars/your-order-please.js
@@ -0,0 +1,18 @@
+// Task: Sort a given string words. Each word in the string will contain a single number. This number tells you which position the word should be placed in the result. Numbers range from 1 -> 9.
+
+function order(words){
+
+  var result = []
+  var arr = words.split(' ')
+
+  for(i = 0; i < arr.length; i++){
+    var key = arr[i].split(/ /)[0].replace(/[^\d]/g, '')
+    // .replace(/[^d]/g,'') --> Replaces all none digits 
+    result[key] = arr[i]
+  }
+
+  return result.join(' ').trim()
+
+}
+
+console.log(order("is2 Thi1s T4est 3a") + " Thi1s is2 3a T4est")
