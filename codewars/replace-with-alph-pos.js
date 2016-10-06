5234f3617c72c23d2b5198acc4ea79cb17bd2205
mattlaguardia <matthewlaguardia@gmail.com>
2016-10-06 16:19:52 -0700

    adds if statement to finish edge case

--- codewars/replace-with-alph-pos.js
+++ codewars/replace-with-alph-pos.js
@@ -10,9 +10,12 @@ function alphPos(text) {
     if(t[i] < 0 ){
       t[i] = ''
     }
+    if(t[i] > 26){
+      t[i] = ''
+    }
   }
   return t.join('').trim()
 
 }
 
-console.log(alphPos("The sunset sets at twelve o' clock."), "SOLUTION: 20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11")
+console.log(alphPos("The sunset sets at twelve o' clockz"), "SOLUTION: 20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11")
