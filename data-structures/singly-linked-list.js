d3909c9c21d03c0bcef20a8dbf3fd825452de99a
mattlaguardia <matthewlaguardia@gmail.com>
2016-11-07 14:00:14 -0800

    adds singly linked list functionality

--- data-structures/singly-linked-list.js
+++ data-structures/singly-linked-list.js
@@ -8,3 +8,27 @@ const SinglyList = () => {
   this._length = 0
   this.head = null
 }
+// Functionality to add nodes to a list //
+
+SinglyList.prototype.add = (value) => {
+  var node = new Node(value)
+  var currentNode = this.head
+
+  // 1st use-case: an empty list //
+  if(!currentNode){
+    this.head = node
+    this._length++
+    return node
+  }
+
+  // 2nd use-case: a non-empty list //
+  while(currentNode.next){
+    currentNode = currentNode.next
+  }
+  
+  currentNode.next = node
+  this._length++
+
+  return node
+
+}
