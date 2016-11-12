ea73f2767d1164866d158d95de2d7d79b19d95d4
mattlaguardia <matthewlaguardia@gmail.com>
2016-11-11 16:24:33 -0800

    updates

--- code-fights/phone-call.js
+++ code-fights/phone-call.js
@@ -1,21 +1,18 @@
 phoneCall = (min1, min2_10, min11, s) => {
   if( s < min1 ){ return 0 }
   if( s === min1 ){ return 1 }
-  var counter = 1
+  var minutes = 1
+  s = s - min1
 
-  while(s > 0){
-    if( counter === 1 ){
-      s = s - min1
-    }
-    if( counter > 10 ){
-      s = s - min11
-    }
-    if( counter <= 10 ){
+  while(s >= 0){
+    if(minutes <= 10){
       s = s - min2_10
+        minutes++
     }
-    counter++
+    s = s - min11
+    minutes++
   }
-  return counter
+  return minutes
 }
 
 console.log(phoneCall(3, 1, 2, 20), "Answer: 14")
