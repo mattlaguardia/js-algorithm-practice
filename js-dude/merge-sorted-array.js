7e46de3cb92118f737026f44f5d4387426d98f0b
mattlaguardia <matthewlaguardia@gmail.com>
2016-11-22 16:00:36 -0800

    finishes mereged arrays

--- js-dude/merge-sorted-array.js
+++ js-dude/merge-sorted-array.js
@@ -9,9 +9,19 @@ mergeSortedArray = (arrA, arrB) => {
       return result = arrA.concat(arrB)
     }
 
-    while(){
+// if aElm or bElm exists we will insert to merged array (will go inside while loop).. to insert: aElm exists and bElm doesn't exist or both exist and aElm < bElm.. this is the critical part of the example //
 
+    while(aElm || bElm){
+      if(aElm && !bElm || aElm < bElm){
+        result.push(aElm)
+        aElm = arrA[i++]
+      }
+      else {
+        result.push(bElm)
+        bElm = arrB[j++]
+      }
     }
+  return result
 }
 
-console.log(mergeSortedArray([2,5,6,9], [1,2,3,29]) " = [1, 2, 2, 3, 5, 6, 9, 29]")
+console.log(mergeSortedArray([2,5,6,9], [1,2,3,29]), " = [1, 2, 2, 3, 5, 6, 9, 29]")
