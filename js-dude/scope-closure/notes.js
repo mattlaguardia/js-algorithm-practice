5238835064301845909d13e72e352ee3921d86df
mattlaguardia <matthewlaguardia@gmail.com>
2016-12-22 18:10:51 -0800

    adds some notes from js dude

---
+++ js-dude/scope-closure/notes.js
@@ -0,0 +1,20 @@
+
+
+///////////////////
+// Global Scopes
+///////////////////
+
+// global variable
+var a = 2
+
+// global function
+function b() {
+  console.log(a) // accesses global variable
+}
+
+// execute function
+b()
+
+///////////////////
+// Local Scopes
+///////////////////
